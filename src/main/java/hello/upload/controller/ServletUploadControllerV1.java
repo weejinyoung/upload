3a7a9cc4d7571a97d63118d01a273c337c223d9b
Jn W <a8118199@gmail.com>
2022-04-28 22:26:19 +0900

    파일 업로드 초기세팅

---
+++ src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,17 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    @GetMapping
+    public String newFile() {
+        return "upload-form";
+    }
+}
